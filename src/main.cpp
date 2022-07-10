dee725acaf679425873fb352a41feadb37bf9749
nicoripkip <nicovanommen.nvo@gmail.com>
2022-07-10 14:51:32 +0200

    Multiplatform support!

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,49 @@
+/** ===============================================================================================
+ * @file main.cpp
+ * @author Niko van Ommen (nikovanommen.nvo@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-07-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * =============================================================================================== */ 
+
+
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+
+#define RUN_FLAG true
+
+
+void run();
+
+
+/**
+ * @brief Heart of the program
+ * 
+ * @return int 
+ */
+int main() 
+{
+    pinMode(2, OUTPUT);
+
+    Serial.begin(115200);
+
+    run();
+
+    return 0;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+/**
+ * @brief Function for repeating functions of the operating system
+ * 
+ */
+void run()
+{
+    while (RUN_FLAG) {
+        Serial.println("[info]\t\tMain program runs!!!");
+        delayMicroseconds(500);
+    }
 }
