d4751703d3cec893ffbc98175a4babdeb61bd3fc
nicoripkip <nicovanommen.nvo@gmail.com>
2022-07-11 23:17:55 +0200

    Opzet werkgeheugen!

---
+++ src/memory/memory.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef __NICOS__SYSTEM__MEMORY__
+#define __NICOS__SYSTEM__MEMORY__
+
+
+namespace NICOS__Memory
+{
+    /**
+     * @brief 
+     * 
+     */
+    struct Block_t
+    {
+
+    };
+}
+
+
+#endif
